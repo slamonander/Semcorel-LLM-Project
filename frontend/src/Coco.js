3720fa7cbfafca973a51538faa826e6bcfda7bc8
Spawn7o7 <122921549+Spawn7o7@users.noreply.github.com>
2024-11-12 13:27:13 -0800

    'CoCo' bot animation and css
    css needs to be updated to make CoCo circular, there is a current visual glitch for CoCo on the main app.js page

---
+++ frontend/src/Coco.js
@@ -0,0 +1,16 @@
+// src/CoCo.js
+import React from 'react';
+import './Coco.css';
+
+const Coco = () => {
+  return (
+    <div className="outline">
+        <div className="coco">
+        <div className="eye left-eye"></div>
+        <div className="eye right-eye"></div>
+        </div>
+    </div>
+  );
+};
+
+export default Coco;
